d9b4f04fdbb34815fcf49cf84a0ade8dfe4cef09
Alexey Maksutov <alexey.maksutov@gmail.com>
2020-01-11 16:08:42 -0500

    pass signals to the binary fix

--- 5.7/init/99-mysqld.sh
+++ 5.7/init/99-mysqld.sh
@@ -2,7 +2,7 @@
 
 test -f /var/lib/mysql/auto.cnf || mysqld --user="$PUSER" --initialize --init-file=/usr/share/mysql/init.sql
 
-trap 'kill -TERM $PID; wait $PID' INT
+trap 'kill -TERM $PID; wait $PID' TERM INT TSTP QUIT
 mysqld --user="$PUSER" &
 PID=$!
 wait $PID
